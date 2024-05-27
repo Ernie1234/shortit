c38b76c375091f047758d718b28d234a7a2bdf2e
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-05-27 23:42:17 +0100

    feat: add backend setup with typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("first");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//For env File
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is started at port: http://localhost:${port}`);
+});
