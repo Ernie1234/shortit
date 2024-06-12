d3606700df616f504d50c4d9c8f6aa7741c3a498
John Oshalusi <john.oshalusi@iohk.io>
2024-06-12 08:05:59 +0100

    fix: resolve eslint issues

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -9,8 +9,8 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
 });
 
 app.listen(port, () => {
