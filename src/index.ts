e25d8933fef8cfa6a7499a07343f39aea1895493
Anumah Joshua <88411364+Ernie1234@users.noreply.github.com>
2024-06-12 08:52:40 +0100

    Feature/setup configuration (#3)
    * feat:add backend setup with typescript
    
    * feat: add backend setup with typescript
    
    * feat:add eslint setup installation and files
    
    * feat: add setup and configuration files
    
    * fix: resolve eslint issues
    
    ---------
    
    Co-authored-by: John Oshalusi <john.oshalusi@iohk.io>

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is started at port: http://localhost:${port}`);
+});
