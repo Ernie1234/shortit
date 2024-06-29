ecbd1a24cdf3d925efc392c947824f8aad920900
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-06-29 12:23:29 +0100

    feat: update the logger import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import winston from 'winston';
 
-import './logs/logger';
+import logger from './logs/logger';
 
 dotenv.config();
 const app = express();
@@ -12,12 +11,10 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-const basicLogger = winston.loggers.get('basicLogger');
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
 app.listen(port, () => {
-  basicLogger.log('info', `Server is started at port: http://localhost:${port}`);
+  logger.info('info', `Server is started at port: http://localhost:${port}`);
 });
