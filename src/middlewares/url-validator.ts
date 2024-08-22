2261620b697b76e26e873a0b3dba0725fcba9d6e
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-08-22 22:55:42 +0100

    feat: extract the validation as a reusable function

--- src/middlewares/url-validator.ts
+++ src/middlewares/url-validator.ts
@@ -1,19 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { postUrlSchema, updateUrlSchema } from '../validators/url-validator';
+import { validateFn } from '../utils/short-url-generator';
 
 export const validatePostUrl = async (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = postUrlSchema.validate(req.body);
-  if (error) {
-    return res.status(400).send({ message: error.details[0].message });
-  }
-  req.body = value;
-  return next();
+  validateFn(postUrlSchema, req, res, next);
 };
 export const validateUpdateUrl = async (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = updateUrlSchema.validate(req.body);
-  if (error) {
-    return res.status(400).send({ message: error.details[0].message });
-  }
-  req.body = value;
-  return next();
+  validateFn(updateUrlSchema, req, res, next);
 };
