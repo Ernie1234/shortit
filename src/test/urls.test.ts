c11da19cd15f29923d5557373590e2a14b7bff72
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-10-25 21:43:45 +0100

    feat: setup tests

--- src/test/urls.test.ts
+++ src/test/urls.test.ts
@@ -1,6 +1,9 @@
 import { TestFactory } from './factory';
 import HTTP_STATUS from '../utils/http-status';
 
+const url = '/api/v1/shorten';
+const exampleUrl = 'https://www.example.com';
+
 describe('POST /shorten', () => {
   const factory = new TestFactory();
 
@@ -13,7 +16,7 @@ describe('POST /shorten', () => {
   });
 
   it('It creates a short URL successfully', async () => {
-    const response = await factory.app.post('/api/v1/shorten').send({
+    const response = await factory.app.post(`${url}`).send({
       url: 'https://www.google.com/search?q=google+url+shortener&oq=google+url+shortener&aqs=chrome..69i57j0l5.3808j0j7&sourceid=chrome&ie',
     });
 
@@ -22,8 +25,49 @@ describe('POST /shorten', () => {
     expect(response.body.data).toHaveProperty('shortUrl');
   });
 
-  it.todo('It returns a 400 error when the URL is not valid');
-  it.todo('It returns a 400 error when the URL is not provided');
-  it.todo('It returns a 400 error when the custom name is not valid');
-  it.todo('It returns a 400 error when the URL already exists');
+  it('It returns a 400 error when the URL is not valid', async () => {
+    const response = await factory.app.post(`${url}`).send({
+      url: 'invalid-url',
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.message).toBe('Invalid URL format');
+  });
+
+  it('It returns a 400 error when the URL is not provided', async () => {
+    const response = await factory.app.post(`${url}`).send({});
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.message).toBe('URL is required');
+  });
+
+  it('It returns a 400 error when the custom name is not valid', async () => {
+    const response = await factory.app.post(`${url}`).send({
+      url: exampleUrl,
+      customName: 'inv', // Example of an invalid custom name
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.message).toBe('Must be at least 5 letters');
+  });
+
+  it('It returns a 400 error when the URL already exists', async () => {
+    // First, create a URL
+    await factory.app.post(`${url}`).send({
+      url: exampleUrl,
+    });
+
+    // Then, try to create it again
+    const response = await factory.app.post(`${url}`).send({
+      url: exampleUrl,
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.message).toBe('Url already exists!');
+  });
+
+  // it.todo('It returns a 400 error when the URL is not valid');
+  // it.todo('It returns a 400 error when the URL is not provided');
+  // it.todo('It returns a 400 error when the custom name is not valid');
+  // it.todo('It returns a 400 error when the URL already exists');
 });
