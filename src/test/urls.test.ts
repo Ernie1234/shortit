43d5b713df9db82fb4ec7eac5a30473c83cdd7b0
Anumah Joshua <88411364+Ernie1234@users.noreply.github.com>
2024-11-20 10:27:34 +0100

    test: add more get route tests (#10)
    * feat: setup tests
    
    * ffeat:" try fixing the test
    
    * fix: update test for custom error handling in middleware
    
    * feat: invalid url error pending
    
    * fix: resolve url validator issue
    
    * fix: update all test case and successful
    
    * fix: remove all unnecessary comments
    
    ---------
    
    Co-authored-by: John Oshalusi <john.oshalusi@iohk.io>

--- src/test/urls.test.ts
+++ src/test/urls.test.ts
@@ -1,6 +1,9 @@
 import { TestFactory } from './factory';
 import HTTP_STATUS from '../utils/http-status';
 
+const url = '/api/v1/shorten';
+const exampleUrl = 'https://www.example.com';
+
 describe('POST /shorten', () => {
   const factory = new TestFactory();
 
@@ -13,7 +16,7 @@ describe('POST /shorten', () => {
   });
 
   it('It creates a short URL successfully', async () => {
-    const response = await factory.app.post('/api/v1/shorten').send({
+    const response = await factory.app.post(url).send({
       url: 'https://www.google.com/search?q=google+url+shortener&oq=google+url+shortener&aqs=chrome..69i57j0l5.3808j0j7&sourceid=chrome&ie',
     });
 
@@ -22,8 +25,40 @@ describe('POST /shorten', () => {
     expect(response.body.data).toHaveProperty('shortUrl');
   });
 
-  it.todo('It returns a 400 error when the URL is not valid');
-  it.todo('It returns a 400 error when the URL is not provided');
-  it.todo('It returns a 400 error when the custom name is not valid');
-  it.todo('It returns a 400 error when the URL already exists');
+  it('It returns a 400 error when the URL is not valid', async () => {
+    const response = await factory.app.post(`${url}`).send({
+      url: 'invalid-url', // Invalid URL format
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.url).toBe('Invalid URL format. URL must start with http or https');
+  });
+
+  it('It returns a 400 error when the URL is not provided', async () => {
+    const response = await factory.app.post(`${url}`).send({ customName: ' ' });
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.url).toBe('Url is required.');
+  });
+
+  it('It returns a 400 error when the custom name is not valid', async () => {
+    const res1 = await factory.app
+      .post(`${url}`)
+      .send({ url: 'https://www.youtube.com/watch?v=xZLKALpvdBE', customName: 'cast' });
+    expect(res1.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    // expect(res1.body.errors).toBeDefined(); // Check that errors is defined
+    expect(res1.body.customName).toBe('Must be at least 5 letters');
+  });
+
+  it('It returns a 400 error when the URL already exists', async () => {
+    await factory.app.post(`${url}`).send({
+      url: exampleUrl,
+    });
+
+    const response = await factory.app.post(`${url}`).send({
+      url: exampleUrl,
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
+    expect(response.body.message).toBe('Url already exists!');
+  });
 });
--- src/validators/url-validator.ts
+++ src/validators/url-validator.ts
@@ -1,13 +1,13 @@
 import Joi from 'joi';
 
+const urlSchema = Joi.string()
+  .uri({ scheme: ['http', 'https'] })
+  .message('Invalid URL format. URL must start with http or https');
+
 export const postUrlSchema = Joi.object({
-  url: Joi.string()
-    .uri({ scheme: ['http', 'https'] })
-    .required()
-    .messages({
-      'string.uri': 'Invalid URL format',
-      'any.required': 'URL is required',
-    }),
+  url: urlSchema.required().messages({
+    'any.required': 'Url is required.',
+  }),
   customName: Joi.string().optional().min(5).message('Must be at least 5 letters'),
 });
 
